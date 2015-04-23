40e7414b4eb90b91ab185eefbe86f5b45e0664ac
Andrea <root@enduavon-Lenovo-G50-70>
2015-04-23 17:34:52 +0200

    session, new servlet "ConfermaParametri" (es4)

--- src/servlet/LeggiParametriProdotto.java
+++ src/servlet/LeggiParametriProdotto.java
@@ -9,9 +9,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
-@WebServlet("/processaDati")
+@WebServlet("/processa")
 public class LeggiParametriProdotto extends HttpServlet {
 private static final long serialVersionUID = 1L;
 			       
@@ -24,14 +25,29 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	String prezzo = request.getParameter("prezzo").toUpperCase();
 	String descrizione = request.getParameter("descrizione").toUpperCase();
 	String codice = request.getParameter("codice").toUpperCase();
+	
+	// metto i valori nella sessione
+			HttpSession session = request.getSession();
+			session.setAttribute("NOME", nome);
+			session.setAttribute("PREZZO", prezzo);
+			session.setAttribute("DESCRIZIONE", descrizione);
+			session.setAttribute("CODICE", codice);
 
 	// leggo (alcune) intestazioni http della richiesta
 	String address = (String)request.getRemoteAddr();
 	String host = (String)request.getRemoteHost(); 
 	String userAgent = request.getHeader("User-Agent");
 	
+	//e li metto nella sessione anch'essi
+	session.setAttribute("address", address);
+	session.setAttribute("host", host);
+	session.setAttribute("userAgent" , userAgent);
+	
 	//settare gli attributi, così riesco a richiamarli dall'altra servlet tramite getAttribute??
 	//cerca su internet la documentazione
+	/*
+	 * non serve più metterli nella richiesta, perchè ho messo i dati
+	 nella sessione
 	request.setAttribute("NOME", nome);
 	request.setAttribute("PREZZO", prezzo);
 	request.setAttribute("DESCRIZIONE", descrizione);
@@ -39,12 +55,13 @@ protected void doGet(HttpServletRequest request, HttpServletResponse response)
 	request.setAttribute("ADDRESS", address);
 	request.setAttribute("HOST", host);
 	request.setAttribute("USERAGENT", userAgent);
+	*/
 	
 	//mi faccio ritornare le informazioni di tutta l'applicazione (contesto), e invoco
 	//il dispatcher, grazie al quale inoltro la richiesta tramite il metodo forward.
-	//Quindi in pratica collego le due servlet LeggiParametri e MostraParametri
+	//Quindi in pratica collego la servlet corrente alla servlet di conferma
 	ServletContext application  = getServletContext();
-	RequestDispatcher rd = application.getRequestDispatcher("/mostraDati");
+	RequestDispatcher rd = application.getRequestDispatcher("/conferma");
 	rd.forward(request, response);
 	return; 
  	
--- src/servlet/MostraParametri.java
+++ src/servlet/MostraParametri.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/mostraDati")
 public class MostraParametri extends HttpServlet {
@@ -15,8 +16,10 @@ public class MostraParametri extends HttpServlet {
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-
-	// gestione della RISPOSTA (prima era stata affidata alla classe LeggiParametri) 
+		
+		HttpSession session = request.getSession();
+		
+	// gestione della RISPOSTA (prima  era stata affidata alla classe LeggiParametri) 
 	
 		// preparo il tipo (HTML)
 				response.setContentType("text/html");
@@ -32,15 +35,15 @@ public class MostraParametri extends HttpServlet {
 			    	out.println("<body>");
 			    	out.println("<h1>Dati inseriti nella form</h1>");
 			    	out.println("<ul>");
-			    	out.println("<li>Nome: <b>" + request.getAttribute("NOME")+ "</b></li>");
-			    	out.println("<li>Prezzo: <b>" + request.getAttribute("PREZZO")+"</b></li>");
-			    	out.println("<li>Descrizione: <b>" + request.getAttribute("DESCRIZIONE")+"</b></li>");
-			    	out.println("<li>Codice: <b>" + request.getAttribute("CODICE")+"</b></li>");
+			    	out.println("<li>Nome: <b>" + session.getAttribute("NOME")+ "</b></li>");
+			    	out.println("<li>Prezzo: <b>" + session.getAttribute("PREZZO")+"</b></li>");
+			    	out.println("<li>Descrizione: <b>" + session.getAttribute("DESCRIZIONE")+"</b></li>");
+			    	out.println("<li>Codice: <b>" + session.getAttribute("CODICE")+"</b></li>");
 			    	out.println("</ul>");
 			    	out.println("<h1>Altri dati relativi alla richiesta</h1>");
-			    	out.println("<br />IP: <b>" + request.getAttribute("ADDRESS")+"</b>");
-			    	out.println("<br />Host: <b>" + request.getAttribute("HOST")+"</b>");
-			    	out.println("<br />User Agent: <b>" + request.getAttribute("USERAGENT")+"</b>");
+			    	out.println("<br />IP: <b>" + session.getAttribute("address")+"</b>");
+			    	out.println("<br />Host: <b>" + session.getAttribute("host")+"</b>");
+			    	out.println("<br />User Agent: <b>" + session.getAttribute("userAgent")+"</b>");
 			    	out.println("</body>\n</html> ");
 			    	out.println("<h2>mynjnj</h2>");
 			}
